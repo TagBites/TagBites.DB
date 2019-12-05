dfa2045d9072b8da3aead820e6c7ba8ad73f9602
Tomasz Malik <tomasz.malik@outlook.com>
2019-12-05 02:53:59 +0100

    feat: disable auto prepared commands

--- src/TagBites.DB.Npgsql/NpgsqlLinkAdapter.cs
+++ src/TagBites.DB.Npgsql/NpgsqlLinkAdapter.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Data.Common;
+﻿using System.Data.Common;
 using Npgsql;
 using NpgsqlTypes;
 using TagBites.DB.Postgres;
@@ -35,6 +34,8 @@ namespace TagBites.DB.Npgsql
 
             sb.Enlist = false;
             sb.Pooling = false;
+            sb.MaxAutoPrepare = 0;
+
             sb.Remove(nameof(NpgsqlConnectionStringBuilder.MinPoolSize));
             sb.Remove(nameof(NpgsqlConnectionStringBuilder.MaxPoolSize));
 
@@ -44,68 +45,18 @@ namespace TagBites.DB.Npgsql
         protected override DbCommand CreateCommand(Query query)
         {
             var cmd = new NpgsqlCommand(query.Command);
-            //cmd.AllResultTypesAreUnknown = true;
 
             foreach (var item in query.Parameters)
             {
                 var value = item.Value;
-                //if (value != null)
-                //{
-                //    Type valueType = value.GetType();
-                //    if (valueType.IsClass && valueType != typeof(string) && !valueType.IsArray)
-                //    {
-                //        var args = GetGenericArguments(valueType, typeof(IEnumerable<>));
-                //        if (args != null && args.Length == 1)
-                //        {
-                //            var array = Array.CreateInstance(valueType, 1);
-                //            array.SetValue(value, 0);
-                //            value = array;
-                //        }
-                //    }
-                //}
-
-                cmd.Parameters.Add(new NpgsqlParameter(item.Name, value ?? DBNull.Value)
-                {
-                    IsNullable = true,
-                    NpgsqlDbType = NpgsqlDbType.Unknown
-                });
+                cmd.Parameters.Add(new NpgsqlParameter(item.Name, value) { NpgsqlDbType = NpgsqlDbType.Unknown });
             }
 
-            //cmd.Prepare();
             return cmd;
         }
         protected override DbDataAdapter CreateDataAdapter(DbCommand command)
         {
             return new NpgsqlDataAdapter((NpgsqlCommand)command);
         }
-
-        //private static Type[] GetGenericArguments(Type type, Type genericTypeDefinition)
-        //{
-        //    var ti = type.GetTypeInfo();
-
-        //    if (ti.IsGenericTypeDefinition && type == genericTypeDefinition)
-        //        return ti.GenericTypeArguments;
-
-        //    if (ti.IsInterface)
-        //    {
-        //        if (ti.IsGenericType && type.GetGenericTypeDefinition() == genericTypeDefinition)
-        //            return ti.GenericTypeArguments;
-        //    }
-        //    else
-        //    {
-        //        for (var it = ti; it != null; it = it.BaseType?.GetTypeInfo())
-        //            if (it.IsGenericType && it.GetGenericTypeDefinition() == genericTypeDefinition)
-        //                return it.GenericTypeArguments;
-        //    }
-
-        //    foreach (var item in ti.ImplementedInterfaces)
-        //    {
-        //        var iti = item.GetTypeInfo();
-        //        if (iti.IsGenericType && item.GetGenericTypeDefinition() == genericTypeDefinition)
-        //            return iti.GenericTypeArguments;
-        //    }
-
-        //    return new Type[0];
-        //}
     }
 }
