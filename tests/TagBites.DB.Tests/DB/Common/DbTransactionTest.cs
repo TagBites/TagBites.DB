47ffcb4c7c5bd9c05436d0686c1a45762a80e145
Tomasz Malik <tomasz.malik@outlook.com>
2020-01-15 01:39:26 +0100

    fix: TransactionBeforeCommit not fired

---
+++ tests/TagBites.DB.Tests/DB/Common/DbTransactionTest.cs
@@ -0,0 +1,33 @@
+﻿using TagBites.DB.Tests.DB.Core;
+using Xunit;
+
+namespace TagBites.DB.Tests.DB.Common
+{
+    public class DbTransactionTest : DbTestBase
+    {
+        [Fact]
+        public void BeforeCommitTest()
+        {
+            var ok = false;
+
+            using (var link = DefaultProvider.CreateLink())
+            using (var transaction = link.Begin())
+            {
+                link.TransactionContext.TransactionBeforeCommit += (s, e) => ok = true;
+                transaction.Commit();
+            }
+
+            Assert.False(ok);
+
+            using (var link = DefaultProvider.CreateLink())
+            using (var transaction = link.Begin())
+            {
+                link.Force();
+                link.TransactionContext.TransactionBeforeCommit += (s, e) => ok = true;
+                transaction.Commit();
+            }
+
+            Assert.True(ok);
+        }
+    }
+}
